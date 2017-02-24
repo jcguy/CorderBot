8d5469daf4c55c850a7b74b7e2021a3a77452768
Corder Guy <corder@tamu.edu>
2017-02-23 21:19:19 -0600

    Remove caching (for now)

--- modules/weather.py
+++ modules/weather.py
@@ -13,15 +13,15 @@ def ktof(k):
 
 
 def get_weather(zipcode):
-    try:
-        with open("weather_cache", "r") as cache_file:
-            cache = cache_file.readlines()
-            if len(cache) != 0:
-                if (time.time() - float(cache[0].strip())) < 10 * 60:
-                    return "".join(cache[1:]) + "\n(cached)"
-    except FileNotFoundError:
-        pass
-
+    # try:
+    #     with open("weather_cache", "r") as cache_file:
+    #         cache = cache_file.readlines()
+    #         if len(cache) != 0:
+    #             if (time.time() - float(cache[0].strip())) < 10 * 60:
+    #                 return "".join(cache[1:]) + "\n(cached)"
+    # except FileNotFoundError:
+    #     pass
+    #
     with open("weather_api_key", "r") as api_key:
         APPID = api_key.readlines()[0].strip()
 
@@ -35,9 +35,9 @@ def get_weather(zipcode):
              "\t{:.1f}°F\t{:.1f}°C\n".format(ktof(json["main"]["temp"]), ktoc(json["main"]["temp"])) + \
              "\t{}% humidity, {}".format(json["main"]["humidity"], json["weather"][0]["description"])
 
-    with open("weather_cache", "w") as cache_file:
-        cache_file.write(str(time.time()) + '\n')
-        cache_file.write(output)
+    # with open("weather_cache", "w") as cache_file:
+    #     cache_file.write(str(time.time()) + '\n')
+    #     cache_file.write(output)
 
     return output
 
